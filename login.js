a281814fa8177921e5e69036f5cb218a75c26d1c
Kimberly Saviano <ksaviano@gmail.com>
2015-10-27 14:47:35 -0600

    Login working (needs CSS)

--- login.js
+++ login.js
@@ -11,10 +11,10 @@ function User() {
 User.counter = 0;
 
 User.prototype.login = function() {
-	$("#loginInterface").hide;
-	$("#newuserInterface").hide;
-	$("#loggedInInterface").show;
-	$("#username").value = this.username;
+	$("#loginInterface").hide();
+	$("#newuserInterface").hide();
+	$("#loggedInInterface").show();
+	$("#username").html(this.username);
 }
 
 User.prototype.newaccount = function(un, pw) {
@@ -22,65 +22,100 @@ User.prototype.newaccount = function(un, pw) {
 	this.password = pw;
 }
 
-$("#loginBtn").click(function(evt) {
-	var un = $("#unBox").value;
-	var pw = $("#pwBox").value;
-	for(var user in users) {
-		if(user.unsername == un && user.password == pw) {
-			var loggedinUser = user;
-			loggedinUser.login();
-			$("#loggedInInterface").userObj = user;
-		} else {
-			alert("Username and password combination not found.");
-			$("#unBox").value = "";
-			$("#pwBox").value = "";
-		}
-	}
-});
+$(document).ready(function() {
+	var holddiv = $("#newuserInterface");
+	holddiv.data("UNcheck", false);
+	holddiv.data("PWcheck", false);
+console.log(holddiv);
 
-$("#newuserBtn").click(function(evt) {
-	$("#loginBtn").hide;
-	$("#newuserBtn").hide;
-	$("#confirmLbl").show;
-	$("#confirmBox").show;
-	$("#newaccountBtn").show;
-	$("#newaccountBtn").activate = false;
-});
-
-$("#unBox").blur(function(evt) {
-	if($("#confirmBox").is(":visible")) {
-		var un = $("#unBox").value;
-		for(var user in users) {
-			if(un == user.username) {
-				alert("Username is already taken, please select another");
-				$("#unBox").border = "red"
+	$("#loginBtn").click(function(evt) {
+	console.log("Login Button clicked.");
+		var un = $("#unBox").val();
+		var pw = $("#pwBox").val();
+		if(un != undefined) {			
+			for(var i = 0; i < users.length; i++) {
+				if(users[i].username == un && users[i].password == pw) {
+					var loggedinUser = users[i];
+					loggedinUser.login();
+					$("#loggedInInterface").userObj = users[i];
+					return loggedinUser;
+				} else {
 			}
 		}
+		alert("Username and password combination not found.");
+		$("#unBox").text = "";
+		$("#pwBox").text = "";
 	}
 });
-
-$("#confirmBox").keyup(function(evt) {
-	var c = evt.target.value;
-	var orig = $("#password").value;
-	if(c.length > 3 && c.length <= orig.length) {
-		//	continue
-		if(c == orig.substr(c.length)) {
-			$("#pwBox").border = "green";
-			$("#confirmBox").border = "green";
-			
-		}
-	} else {
-		//	mark confirm as red
-	}
-});
-
-$("newaccountBtn").click(function(evt) {
-	var newuser = User.newaccount($("#unBox").value, $("#pwBox.value"));
-	users.push(newuser);
-	$("#loginBtn").click();
 	
+	$("#newuserBtn").click(function(evt) {
+		console.log("New User Button clicked.");
+		$("#newuserInterface").show();
+		$("#loginBtn").hide();
+		$("#newuserBtn").hide();
+		$("#confirmLbl").show();
+		$("#confirmBox").show();
+		$("#newaccountBtn").show();
+		$("#newaccountBtn").activate = false;
+	});
+	
+	$("#unBox").blur(function(evt) {
+		$("#unBox").css("border", "black solid 1px");
+		if($("#confirmBox").is(":visible")) {
+			var un = $("#unBox").val();
+			for(var i = 0; i < users.length; i++) {
+				if(un == users[i].username) {
+					alert("Username is already taken, please select another");
+					$("#unBox").css("border", "red solid 2px");
+					$("#newuserInterface").data("UNcheck", false);
+				} else {
+					$("#unBox").css("border", "green solid 2px");
+					$("#newuserInterface").data("UNcheck", true);
+				}
+			}
+		}
+	});
+
+	$("#confirmBox").keyup(function(evt) {
+		var c = evt.target.value;
+		var orig = $("#pwBox").val();
+		if(c.length > 3 && c.length <= orig.length) {
+			//	continue
+			$("#confirmBox").css("border", "yellow solid 2px");
+			var osub = orig.substring(0, c.length);
+			if(c == osub) {
+				//	continue
+				if(c.length == orig.length) {
+					$("#pwBox").css("border", "green solid 2px");
+					$("#confirmBox").css("border", "green solid 2px");
+					holddiv.data("PWcheck", true);
+				} else {
+					//	continue
+				}
+			} else {
+				$("#confirmBox").css("border", "red solid 2px");
+				holddiv.data("PWcheck", false);
+			}
+		} 
+	});
+	
+	$("#newaccountBtn").click(function(evt) {
+		var unchk = holddiv.data("UNcheck");
+		var pwchk = holddiv.data("PWcheck");
+console.log("Create New Account Button clicked. UNcheck = " + unchk + " PWcheck = " + pwchk);
+
+		if(unchk == true && pwchk == true) {
+			var newuser = new User(); 
+			newuser.newaccount($("#unBox").val(), $("#pwBox").val());
+			users.push(newuser);
+console.log(users);
+			$("#loginBtn").click();
+		} else {
+			alert("Please complete username and password fields.");
+		}
+		
+	});
 });
 
 
-		
 
---
+++ logintest.js
@@ -0,0 +1,6 @@
+var user1 = new User();
+user1.newaccount("user1", "password");
+var user2 = new User();
+user2.newaccount("user2", "password");
+users.push(user1);
+users.push(user2);
