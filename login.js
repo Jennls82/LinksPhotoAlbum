639c241b493fb40335b8188ce065352ef2c0dc9d
Kimberly Saviano <ksaviano@gmail.com>
2015-10-27 11:00:01 -0600

    login almost ready for testing

--- login.js
+++ login.js
@@ -1,17 +1,86 @@
+var users = [];
 
 function User() {
-	this.userID;
+	this.userID = User.counter++;
 	this.username;
 	this.password;
 	this.photos = [];
 	this.albums = [];
 }
 
-User.prototype.login = function(un, pw) {
+User.counter = 0;
+
+User.prototype.login = function() {
+	$("#loginInterface").hide;
+	$("#newuserInterface").hide;
+	$("#loggedInInterface").show;
+	$("#username").value = this.username;
+}
+
+User.prototype.newaccount = function(un, pw) {
 	this.username = un;
 	this.password = pw;
 }
 
-User.prototype.newaccount = function(un, pw) {
-	for each (user )
-}
+$("#loginBtn").click(function(evt) {
+	var un = $("#unBox").value;
+	var pw = $("#pwBox").value;
+	for(var user in users) {
+		if(user.unsername == un && user.password == pw) {
+			var loggedinUser = user;
+			loggedinUser.login();
+			$("#loggedInInterface").userObj = user;
+		} else {
+			alert("Username and password combination not found.");
+			$("#unBox").value = "";
+			$("#pwBox").value = "";
+		}
+	}
+});
+
+$("#newuserBtn").click(function(evt) {
+	$("#loginBtn").hide;
+	$("#newuserBtn").hide;
+	$("#confirmLbl").show;
+	$("#confirmBox").show;
+	$("#newaccountBtn").show;
+	$("#newaccountBtn").activate = false;
+});
+
+$("#unBox").blur(function(evt) {
+	if($("#confirmBox").is(":visible")) {
+		var un = $("#unBox").value;
+		for(var user in users) {
+			if(un == user.username) {
+				alert("Username is already taken, please select another");
+				$("#unBox").border = "red"
+			}
+		}
+	}
+});
+
+$("#confirmBox").keyup(function(evt) {
+	var c = evt.target.value;
+	var orig = $("#password").value;
+	if(c.length > 3 && c.length <= orig.length) {
+		//	continue
+		if(c == orig.substr(c.length)) {
+			$("#pwBox").border = "green";
+			$("#confirmBox").border = "green";
+			
+		}
+	} else {
+		//	mark confirm as red
+	}
+});
+
+$("newaccountBtn").click(function(evt) {
+	var newuser = User.newaccount($("#unBox").value, $("#pwBox.value"));
+	users.push(newuser);
+	$("#loginBtn").click();
+	
+});
+
+
+		
+
