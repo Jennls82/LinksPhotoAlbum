86709fe4c979f746036ccecb96790bd39874f41d
Jennifer Sutherland <jennifersutherland@gmail.com>
2015-10-28 11:04:05 -0600

    11am

--- upload.js
+++ upload.js
@@ -1,32 +1,47 @@
 $(document).ready(function(){
-			$( "#accordion" ).accordion();
+			$("#accordion").accordion();
 			$("#date").datepicker();
 			$("#menu").menu();
 			$("input[type=button]").button();
-			
+		     $("#photopath").button();
 			$("#uploadTags").click(
 	function(){
-	var formArray=[];
 	var title = $("#title").val();  
-	var comment = $("#comment").val();  
+	var photopath = $("#photopath").val();
+	var caption = $("#caption").val();  
 	var date = $("#date").val();  
-	var album = [];//$("checkbox").val();
-		$("input[type=checkbox]").each(function(){
-			if ($(this).is(':checked')) {
-       			album.push($(this).val());
-			};
-		})
-		// $("checkbox").on('change', ':checkbox', function () {
-    	// 	if ($(this).is(':checked')) {
-       	// 	album.push(this.val());
-		// 	};
-		//});
-	formArray.push(title,comment,date,album);
+	var metatags= [];//fxn
+	
+	function addPhotoIstance() {
+		var temp = new Photo();
+		temp.newPhoto(photopath);
+		temp.setPhotoTitle(title);
+		temp.setPhotocaption(caption);
+		temp.setPhotoTakenDate(date);
+		//need to set the album info
+		
+	
+	 
+	// var album = [];//$("checkbox").val();
+	// 	$("input[type=checkbox]").each(function(){
+	// 		if ($(this).is(':checked')) {
+    //    			album.push($(this).val());
+	// 		};
+	// 	})
+	// formArray.push(title,caption,date, photoUploadDate,album);
+	
+	// //delete if unchecked
+	// 	function unchecked(){$("input[type=checkbox]").each(function(){
+	// 		if ($(this).is(':checked, false')) {
+    //    			album.delete($(this).val());
+			
+		
+	
 	  
 	JSON.stringify($("#uploadinfo").serializeArray());
-	console.log(formArray);
+	console.log(photos);
+	};		
 	});	
-			
 	});
 	
 	
