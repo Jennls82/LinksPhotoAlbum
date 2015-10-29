0c554d0340daa6ee8823c743d7058167e85a79cf
Jennifer Sutherland <jennifersutherland@gmail.com>
2015-10-28 19:28:33 -0600

    EOD 11/28

---
+++ displayalbum.js
@@ -0,0 +1,62 @@
+function readyDAdiv(album) {
+
+
+
+//(function cycle(){   
+//     console.log("in fxn cycle");
+// var albumphotos = album.photos;   
+// var i = 0;
+// var disPic = albumphotos[i];
+//     $("#phototitle").html(disPic[i].phototitle);
+//     $("#picArea").html("<img src='" + disPic[i].photopath + "' class='images'>");
+//     $("#captionText").html(disPic[i].photocaption);
+
+
+//     console.log("in fxn Cycle");
+//      while(i < albumphotos.length){
+//          i++;
+//          disPic = albumphotos[i];
+//          $("#phototitle").html(disPic[i].phototitle);
+//          $("#picArea").html("<img src='" + disPic[i].photopath + "' class='images' data-i='"+ i +"'>");
+//          $("#captionText").html(disPic[i].photocaption);   
+//      }
+//      cycle();  
+//   })();//end cycle fxn
+  
+ //if next is selected stop cycle();
+ $("#next").click(function cyclePicsF(){
+     console.log("in fxn cyclePicsF");
+    var albumphotos = album.photos;   
+    var i = Number($("#picArea").data-i);
+    var disPic = albumphotos[i];
+        $("#phototitle").html(disPic[i].phototitle);
+        $("#picArea").html("<img src='" + disPic[i].photopath);
+        $("#captionText").html(disPic[i].photocaption);
+        
+        if(i==0){
+            i=albumphotos.length;           
+         $("#phototitle").html(disPic[i].phototitle);
+         $("#picArea").html("<img src='" + disPic[i].photopath + "' class='images'>");
+         $("#captionText").html(disPic[i].photocaption);   
+     }
+    });
+    
+ $("#previous").click(function cyclePicsB(){
+        console.log("in fxn B");
+     var albumphotos = album.photos;   
+     var i = Number($("#picArea").data-i);
+     var disPic = albumphotos[i];
+        $("#phototitle").html(disPic[i].phototitle);
+        $("#picArea").html("<img src='" + disPic[i].photopath + "' class='images'>");
+        $("#captionText").html(disPic[i].photocaption);
+        
+        //current position
+        if(i==0){
+            i=albumphotos.length;           
+         $("#phototitle").html(disPic[i].phototitle);
+         $("#picArea").html("<img src='" + disPic[i].photopath + "' class='images'>");
+         $("#captionText").html(disPic[i].photocaption);   
+     }
+    });
+};
+
